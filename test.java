db37c22dd3a3884a7d1bd1e17452d1a07e2cab4f
gilamui <mattew_marante_33rpl@student.smktelkom-mlg.sch.id>
2024-08-02 13:58:19 +0700

    Add files via upload

---
+++ test.java
@@ -0,0 +1,6 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Nama saya Gilam dari kelas X RPL 7 yang berasal dari kota timika");
+        String a = "1"; 
+    }
+}
