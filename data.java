1a2dc731ca3eb528bd5e72c617db9d95c8a4b8ef
gilamui <mattew_marante_33rpl@student.smktelkom-mlg.sch.id>
2024-08-05 11:37:13 +0700

    Add files via upload

---
+++ data.java
@@ -0,0 +1,16 @@
+public class data {
+    public static void main(String[] args) {
+        //string = kata, kalimat, paragraf
+        String nama = "Gilam";
+        //int = angka bulat
+        int usia = 15;
+        String alamat = "JL.Segara Anak Sawojajar 1";
+        //double = angka pecahan atau desimal
+        double kelas = 10.5;
+
+        System.out.println("Nama saya " + nama);
+        System.out.println("berusia " + usia + " tahun");
+        System.out.println("tinggal di " + alamat);
+        System.out.println("Kelas " + kelas);
+    }
+}
